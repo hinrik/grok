69d5fe811088e21a5fceb3ea7ddbb26235777466
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2009-06-26 04:28:10 +0000

    Add some missing Pod boilerplate

--- lib/App/Grok.pm
+++ lib/App/Grok.pm
@@ -122,3 +122,21 @@ sub render_file {
 }
 
 1;
+
+=head1 NAME
+
+App::Grok - Does most of grok's heavy lifting
+
+=head1 AUTHOR
+
+Hinrik Örn Sigurðsson, L<hinrik.sig@gmail.com>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2009 Hinrik Örn Sigurðsson
+
+C<grok> is distributed under the terms of the Artistic License 2.0.
+For more details, see the full text of the license in the file F<LICENSE>
+that came with this distribution.
+
+=cut
--- lib/App/Grok/Pod6.pm
+++ lib/App/Grok/Pod6.pm
@@ -27,3 +27,21 @@ sub render {
 }
 
 1;
+
+=head1 NAME
+
+App::Grok::Pod6 - A Pod 6 backend for grok
+
+=head1 AUTHOR
+
+Hinrik Örn Sigurðsson, L<hinrik.sig@gmail.com>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2009 Hinrik Örn Sigurðsson
+
+C<grok> is distributed under the terms of the Artistic License 2.0.
+For more details, see the full text of the license in the file F<LICENSE>
+that came with this distribution.
+
+=cut
