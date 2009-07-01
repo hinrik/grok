a8f879c84cfdc1113a9e5ea3b1541ca2983eef47
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2009-07-01 14:14:19 +0000

    Bump version to 0.08

--- lib/App/Grok.pm
+++ lib/App/Grok.pm
@@ -11,7 +11,7 @@ use Getopt::Long qw<:config bundling>;
 use List::Util qw<first>;
 use Pod::Usage;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 my %opt;
 
 sub new {
--- lib/App/Grok/Pod6.pm
+++ lib/App/Grok/Pod6.pm
@@ -6,7 +6,7 @@ use Perl6::Perldoc::Parser;
 use strict;
 use warnings;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 sub new {
     my ($package, %self) = @_;
