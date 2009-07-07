f7c6085b61a6a2ff25f34b6025664077c42f8bb7
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2009-07-06 23:36:27 -0400

    Bump version to 0.11

--- lib/App/Grok/Pod5.pm
+++ lib/App/Grok/Pod5.pm
@@ -3,7 +3,7 @@ package App::Grok::Pod5;
 use strict;
 use warnings;
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 my %formatter = (
     text  => 'Pod::Text',
--- lib/App/Grok/Pod6.pm
+++ lib/App/Grok/Pod6.pm
@@ -6,7 +6,7 @@ use Perl6::Perldoc::Parser;
 use strict;
 use warnings;
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 sub new {
     my ($package, %self) = @_;
